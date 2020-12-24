ffc743fd5e8f28c6e27d14e8fbd3dbf4f7b109d2
geqipeng <geqipeng@sina.cn>
2020-12-24 17:07:17 +0800

    添加滑动控制alpha值

--- app/src/main/java/com/example/myapplication/touchablePop/TouchablePopUpWindow.kt
+++ app/src/main/java/com/example/myapplication/touchablePop/TouchablePopUpWindow.kt
@@ -1,7 +1,9 @@
 package com.example.myapplication.touchablePop
 
 import android.content.Context
-import android.graphics.drawable.BitmapDrawable
+import android.os.Build
+import android.util.DisplayMetrics
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.WindowManager
@@ -9,6 +11,7 @@ import android.widget.ImageView
 import android.widget.PopupWindow
 import android.widget.Toast
 import com.example.myapplication.R
+import java.lang.reflect.Field
 import kotlin.math.abs
 
 /**
@@ -22,6 +25,7 @@ class TouchablePopUpWindow constructor(
 
     private var mContext = context
     private var mStartY: Int = 0
+    private var mStartYF: Float = 0f
     private var mDiffY: Int = 0
 
     init {
@@ -32,6 +36,7 @@ class TouchablePopUpWindow constructor(
         val mIvTop = mContentView.findViewById<ImageView>(R.id.iv_top)
         animationStyle = R.style.PopupWindowAnimation
         contentView = mContentView
+        fitPopupWindowOverStatusBar()
         isClippingEnabled = false
 
         mIvTop.setOnTouchListener { _, event ->
@@ -39,11 +44,21 @@ class TouchablePopUpWindow constructor(
                 when (action) {
                     MotionEvent.ACTION_DOWN -> {
                         mStartY = rawY.toInt()
+                        mStartYF = rawY
                     }
                     MotionEvent.ACTION_MOVE -> {
                         mDiffY = rawY.toInt() - mStartY
-                        // 限制方向
-                        if (mDiffY > 0) mDiffY = 0
+                        if (mDiffY < 0) {
+                            var mAlpha =
+                                1 - (abs(rawY - mStartYF) / (getDisplayMetrics(mContext).widthPixels) * 1f)
+                            if (mAlpha < 0f) mAlpha = 0f
+                            if (mAlpha > 1f) mAlpha = 1f
+                            mIvTop.alpha = mAlpha
+                            Log.d("peter", "mAlpha=== $mAlpha")
+                        } else {
+                            // 限制方向
+                            mDiffY = 0
+                        }
                         update(0, -mDiffY, -1, -1, true)
                     }
                     MotionEvent.ACTION_UP -> {
@@ -59,6 +74,30 @@ class TouchablePopUpWindow constructor(
             }
             true
         }
+
+
+    }
+
+    private fun fitPopupWindowOverStatusBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            try {
+                val mLayoutInScreen: Field =
+                    PopupWindow::class.java.getDeclaredField("mLayoutInScreen")
+                mLayoutInScreen.isAccessible = true
+                mLayoutInScreen.set(this, true)
+            } catch (e: NoSuchFieldException) {
+                e.printStackTrace()
+            } catch (e: IllegalAccessException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun getDisplayMetrics(context: Context): DisplayMetrics {
+        val displayMetrics = DisplayMetrics()
+        (context.applicationContext.getSystemService(Context.WINDOW_SERVICE) as WindowManager)
+            .defaultDisplay.getMetrics(displayMetrics)
+        return displayMetrics
     }
 
 }
